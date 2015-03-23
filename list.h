acaa8c476880b42a4e426ac5687cdfef41489f26
Tutku <tutkutekinel@gmail.com>
2015-03-23 17:56:21 -0400

    list.c list.h main.c added

---
+++ list.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+#define BOOLEAN int
+#define TRUE 1
+#define FALSE 0
+#define MAIN int main(void)
---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <list.h>
+
+MAIN
+{
+	char fname[20];
+	printf("Please enter a file name or a path to a file: ");
+	scanf("%s", fname);
+
+	FILE *data = fopen(fname, "rt");
+
+	int value;
+	fscanf(data, "%d", &value);
+	while (!feof(data))
+	{
+		printf("%d", value);
+		fscanf(data, "%d", &value);	
+	}
+	return 0;
+}
